63a0933c97d945f87aa885143fbfcb40aab93362
Alba <alba__22@outlook.com>
2021-01-13 13:59:09 -0300

    added Auth Service

---
+++ backend/src/services/auth_service.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const jwt = require("jsonwebtoken");
+
+exports.generateToken = async (tokenData) => {
+  return jwt.sign(tokenData, global.PRIVATE_KEY, { expiresIn: "30m" });
+}
+
+exports.decodeToken = async (token) => {
+  return jwt.verify(token, global.PRIVATE_KEY);
+}
+
+exports.authorize = (request, response, next) => {
+  let token = request.headers["x-access-token"];
+
+  if (!token) {
+    response.status(401).json({
+      message: "Restricted Acess. You need to login first",
+    });
+  }
+  else {
+    jwt.verify(
+      token, 
+      global.PRIVATE_KEY, 
+      (error, _) => {
+        if (error) {
+          response.status(401).json({
+            message: "Invalid token"
+          });
+        }
+        else {
+          next();
+        }
+      }
+    );
+  }
+}
